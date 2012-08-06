fad115d088c9b1dff95dd443a34fea84b60117ef
Michał Czerski <czers@users.noreply.github.com>
2012-08-06 23:58:19 +0200

    Squashed commit of the following:
    commit XXXXX
    Author: Michał Czerski <czers@users.noreply.github.com>
    Date:   Mon Aug 6 23:47:14 2012 +0200
    
        Added code for reading input
    
    commit XXXXX
    Author: Michał Czerski <czers@users.noreply.github.com>
    Date:   Mon Aug 6 22:29:58 2012 +0200
    
        Initial commit for o09e2dzi task

---
+++ OI09_Dzialka/dzi.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <stack>
+
+const int kMaxN = 2000;
+
+using std::stack;
+
+int main()
+{
+    int n, lines_left;
+    int best_rectangle = 0;
+    int usable_up[kMaxN + 2];
+    for (int i = 0; i < kMaxN + 2; i++)
+        usable_up[i] = 0;
+    scanf("%d", &n);
+    lines_left = n;
+    while (lines_left--)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            int busy;
+            scanf("%d", &busy);
+            if (busy)
+                usable_up[i] = 0;
+            else
+                ++usable_up[i];
+        }
+    }
+    return 0;
+}
