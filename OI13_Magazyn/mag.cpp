5aa0b20b42bf64de7fdaf768cfbdb8534d91c82f
Michał Czerski <czers@users.noreply.github.com>
2012-08-07 14:29:34 +0200

    Changed usage of LL to int64_t

--- OI13_Magazyn/mag.cpp
+++ OI13_Magazyn/mag.cpp
@@ -1,8 +1,7 @@
+#include <stdint.h>
 #include <cstdio>
 #include <limits>
 #include <algorithm>
-
-typedef long long LL;
 
 using std::sort;
 using std::max;
@@ -23,29 +22,29 @@ class Shop {
 
     static const int kInitX = 0;
     static const int kInitY = 0;
-    static const int kInitVisitCount = -1LL;
+    static const int kInitVisitCount = -1;
 
     Shop() : x_(kInitX), y_(kInitY), visit_count_(kInitVisitCount) {}
 
     int x() const { return x_; }
     int y() const { return y_; }
-    LL visit_count() const { return visit_count_; }
+    int visit_count() const { return visit_count_; }
     static int GetX(const Shop &s) { return s.x_; }
     static int GetY(const Shop &s) { return s.y_; }
-    void Init(int x, int y, LL visit_count);
+    void Init(int x, int y, int visit_count);
     void Rotate();
 
   private:
     int x_;
     int y_;
-    LL visit_count_;
+    int visit_count_;
 };
 
 bool Shop::Comparer::operator()(const Shop &a, const Shop &b) {
     return (*coord_accessor_)(a) < (*coord_accessor_)(b);
 }
 
-void Shop::Init(int x, int y, LL visit_count) {
+void Shop::Init(int x, int y, int visit_count) {
     x_ = x;
     y_ = y;
     visit_count_ = visit_count;
@@ -57,41 +56,41 @@ void Shop::Rotate() {
     y_ = x + y_;
 }
 
-int GetNthCoord(const Shop sorted_shops[], LL nth,
+int GetNthCoord(const Shop sorted_shops[], int64_t nth,
         int (*coord_accessor)(const Shop&)) {
     int i = 0;
     int retval = 0;
-    LL shops_passed = 0;
+    int64_t shops_passed = 0;
     while (shops_passed < nth) {
         retval = (*coord_accessor)(sorted_shops[i]);
-        shops_passed += sorted_shops[i].visit_count();
+        shops_passed += static_cast<int64_t>(sorted_shops[i].visit_count());
         ++i;
     }
     return retval;
 }
 
-int GetMedianCoord(Shop shops[], int n, LL median_visit,
+int GetMedianCoord(Shop shops[], int n, int64_t median_visit,
         int (*coord_accessor)(const Shop&)) {
     Shop::Comparer shop_comparer = Shop::Comparer(coord_accessor);
     sort(shops, shops + n, shop_comparer);
     return GetNthCoord(shops, median_visit, coord_accessor);
 }
 
-LL Distance(LL x1, LL y1, LL x2, LL y2) {
+int64_t Distance(int64_t x1, int64_t y1, int64_t x2, int64_t y2) {
     return max(abs(x1 - x2), abs(y1 - y2));
 }
 
-LL TotalDistance(int x, int y, const Shop shops[], int n) {
-    LL total = 0;
+int64_t TotalDistance(int x, int y, const Shop shops[], int n) {
+    int64_t total = 0;
     for (int i = 0; i < n; ++i)
-        total += shops[i].visit_count() * Distance(x, y, shops[i].x(),
-                                                   shops[i].y());
+        total += static_cast<int64_t>(shops[i].visit_count()) *
+                Distance(x, y, shops[i].x(), shops[i].y());
     return total;
 }
 
 int main() {
     int n = 0;
-    LL total_visit_count = 0;
+    int64_t total_visit_count = 0;
     Shop shops[kMaximumShopsNumber];
     Shop rotated_shops[kMaximumShopsNumber];
 
@@ -105,7 +104,7 @@ int main() {
         rotated_shops[i].Rotate();
     }
 
-    const LL median_visit = (total_visit_count + 1) / 2;
+    const int64_t median_visit = (total_visit_count + 1) / 2;
     const int best_x_rotated = GetMedianCoord(rotated_shops, n,
                                               median_visit,
                                               &Shop::GetX);
@@ -118,12 +117,12 @@ int main() {
     const int ys[] = { 0, 1, 1, 0 };
     int best_x = x;
     int best_y = y;
-    LL lowest_total_distance = numeric_limits<LL>::max();
+    int64_t lowest_total_distance = numeric_limits<int64_t>::max();
     for (int i = 0; i < 4; i++) {
         int current_x = x + xs[i];
         int current_y = y + ys[i];
-        LL current_distance = TotalDistance(current_x, current_y, shops, n);
-        //printf("%d %d TD: %lu\n", current_x, current_y, current_distance);
+        int64_t current_distance = TotalDistance(current_x, current_y,
+                                                 shops, n);
         if (current_distance < lowest_total_distance) {
             lowest_total_distance = current_distance;
             best_x = current_x;
