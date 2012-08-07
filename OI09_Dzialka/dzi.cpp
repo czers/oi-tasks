ff0e86206d06bb4861bce1e109e26ef262e4af03
Michał Czerski <czers@users.noreply.github.com>
2012-08-07 16:56:07 +0200

    Changed opening braces formatting style

--- OI09_Dzialka/dzi.cpp
+++ OI09_Dzialka/dzi.cpp
@@ -1,12 +1,13 @@
 #include <cstdio>
 #include <stack>
+#include <utility>
 
 const int kMaxN = 2000;
 
 using std::stack;
+using std::pair;
 
-int main()
-{
+int main() {
     int n, lines_left;
     int best_rectangle = 0;
     int usable_up[kMaxN + 2];
@@ -14,10 +15,8 @@ int main()
         usable_up[i] = 0;
     scanf("%d", &n);
     lines_left = n;
-    while (lines_left--)
-    {
-        for (int i = 1; i <= n; i++)
-        {
+    while (lines_left--) {
+        for (int i = 1; i <= n; ++i) {
             int busy;
             scanf("%d", &busy);
             if (busy)
