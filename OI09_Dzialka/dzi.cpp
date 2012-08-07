40b89e491017f57526c5b994c0b9f5805fef908b
Michał Czerski <czers@users.noreply.github.com>
2012-08-08 00:21:13 +0200

    Finished.

--- OI09_Dzialka/dzi.cpp
+++ OI09_Dzialka/dzi.cpp
@@ -25,50 +25,20 @@ int main() {
             else
                 ++usable_up[i];
         }
-        printf("Line: %d Usable:", lines_left);
-        for (int i = 0; i < n + 2; i++)
-            printf(" %d", usable_up[i]);
-        printf("\n");
         stack<pair<int, int> > candidates;
-        //candidates.push(make_pair<int, int>(0, 0));
-        pair<int, int> first_candidate;
-        pair<int, int> second_candidate;
+        candidates.push(make_pair(0, 0));
         for (int i = 1; i <= n + 1; ++i) {
-            bool popped = false;
-            if (!candidates.empty())
-            {
-                first_candidate = candidates.top();
-                printf("1ToppedA: %d,%d\n", first_candidate.first,
-                       first_candidate.second);
-            }
-            while (!candidates.empty() && first_candidate.second >
-                   usable_up[i]) {
-                int possible_best = (i - first_candidate.first) *
-                    usable_up[i];
+            int new_start = i;
+            while (usable_up[i] < candidates.top().second) {
+                pair<int, int> candidate = candidates.top();
+                candidates.pop();
+                int possible_best = candidate.second * (i - candidate.first);
                 if (possible_best > best_rectangle)
                     best_rectangle = possible_best;
-                printf("Possible best rectangle (i:%d,G:%d): %d\n", i,
-                       usable_up[i], possible_best);
-                second_candidate = candidates.top();
-                candidates.pop();
-                popped = true;
-                printf("2Popped: %d,%d\n", second_candidate.first,
-                       second_candidate.second);
-                if (!candidates.empty()) {
-                    first_candidate = candidates.top();
-                    printf("1ToppedB: %d,%d\n", first_candidate.first,
-                           first_candidate.second);
-                }
+                new_start = candidate.first;
             }
-            if ((candidates.empty() && usable_up[i] > 0) ||
-                    first_candidate.second < usable_up[i]) {
-                if (popped)
-                    candidates.push(make_pair<int, int>(second_candidate.first, usable_up[i]));
-                else
-                    candidates.push(make_pair<int, int>(i, usable_up[i]));
-                printf("Pushed: %d,%d\n", candidates.top().first,
-                       candidates.top().second);
-            }
+            if (candidates.top().second < usable_up[i])
+                candidates.push(make_pair(new_start, usable_up[i]));
         }
     }
     printf("%d\n", best_rectangle);
