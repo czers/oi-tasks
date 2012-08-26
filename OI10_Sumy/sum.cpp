e5a82b66678d643dc9bdf53e4bc6891892f29327
Michał Czerski <czers@users.noreply.github.com>
2012-08-26 13:33:41 +0200

    Added working solution for o10e3sum task: Sumy

--- OI10_Sumy/sum.cpp
+++ OI10_Sumy/sum.cpp
@@ -1,80 +1,71 @@
 #include <cstdio>
-#include <queue>
-#include <vector>
 #include <limits>
+#include <set>
+#include <algorithm>
 
-const int MAXN = 5000;
+const int kMaxN = 5000;
+const int kMaxA = 50000;
 
-class GraphNode
-{
-    public:
-        int node;
-        int cost;
-        GraphNode(int node, int cost)
-        {
-            this->node = node;
-            this->cost = cost;
-        }
-};
+int min_costs[kMaxA];
 
-struct GraphNodeCompare {
-    bool operator()(const GraphNode &a, const GraphNode &b)
-    {
-        if (a.cost != b.cost)
-            return (a.cost > b.cost);
-        return a.node > b.node;
+struct NodeCompare {
+    bool operator()(const int &a, const int &b) {
+        if (min_costs[a] != min_costs[b])
+            return (min_costs[a] < min_costs[b]);
+        return a < b;
     }
 };
 
-typedef std::priority_queue<GraphNode, std::vector<GraphNode>,
-        GraphNodeCompare> DijkstraQueue;
+typedef std::set<int, NodeCompare> DijkstraSet;
 
 int main() {
     int n;
-    int a[MAXN];
+    int a[kMaxN];
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
     const int min_a = a[0];
-    bool used_modulo[min_a];
-    std::vector<int> edges[min_a];
-    std::vector<int> costs[min_a];
-    for (int i = 0; i < n; i++) {
-        const int mod_a = a[i] % min_a;
-        if (!used_modulo[mod_a]) {
-            used_modulo[mod_a] = true;
-            for (int j = 0; j < min_a; j++) {
-                const int modulo_jump = (j + a[i]) % min_a;
-                edges[j].push_back(modulo_jump);
-                costs[j].push_back(a[i]);
-            }
-        }
-    }
-    DijkstraQueue Q;
-    bool visited[min_a];
-    int min_costs[min_a];
+    // Initialize minimal costs
     for (int i = 0; i < min_a; i++)
         min_costs[i] = std::numeric_limits<int>::max();
-    Q.push(GraphNode(0, 0));
-    while (!Q.empty()) {
-        GraphNode v = Q.top();
-        printf("POP: %d c: %d\n", v.node, v.cost);
-        Q.pop();
-        if (!visited[v.node]) {
-            visited[v.node] = true;
-            min_costs[v.node] = v.cost;
-            for (int i = 0; i < edges[v.node].size(); i++) {
-                if (!visited[edges[v.node][i]])
-                {
-                    Q.push(GraphNode(edges[v.node][i],
-                                     v.cost + costs[v.node][i]));
-                    printf("PUSH: %d c: %d\n", edges[v.node][i],
-                            v.cost + costs[v.node][i]);
+    min_costs[0] = 0;
+    // Find optimal edges
+    int edges[kMaxA];
+    for (int i = 0; i < min_a; i++)
+        edges[i] = std::numeric_limits<int>::max();
+    for (int i = 0; i < n; i++) {
+        const int mod_a = a[i] % min_a;
+        edges[mod_a] = std::min(edges[mod_a], a[i]);
+    }
+    edges[0] = std::numeric_limits<int>::max();
+    std::sort(edges, edges + min_a);
+    const int edges_count = std::lower_bound(edges, edges + min_a,
+            std::numeric_limits<int>::max()) - edges;
+    // Init visited info for Dijkstra algorithm
+    bool visited[kMaxA];
+    for (int i = 0; i < min_a; i++)
+        visited[i] = false;
+    // Run Dijkstra
+    DijkstraSet S;
+    S.insert(0);
+    while (!S.empty()) {
+        const int v = *S.begin();
+        S.erase(S.begin());
+        visited[v] = true;
+        for (int i = 0; i < edges_count; i++) {
+            const int jump = (v + edges[i]) % min_a;
+            if (!visited[jump]) {
+                const int new_cost = min_costs[v] + edges[i];
+                if (new_cost < min_costs[jump]) {
+                    S.erase(jump);
+                    min_costs[jump] = new_cost;
+                    S.insert(jump);
                 }
             }
         }
     }
+    // Print results
     int k;
     scanf("%d", &k);
     while (k--) {
